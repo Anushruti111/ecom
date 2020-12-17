66d10a022cf7ac093aa89bad2a7a11de06e36a0f
Anushruti <67314049+Anushruti111@users.noreply.github.com>
2020-12-17 22:04:12 +0530

    checking

--- mycart/settings.py
+++ mycart/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9h_#wy*6)%#ug3-uv@7xlryan5a36rqe^j5a$-i0@fo9szu=%n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
