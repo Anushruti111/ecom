067bee36295ff6ca00a7dfd5ea0eaa224ad73978
Anushruti111 <anushruti.18bci1034@abes.ac.in>
2020-12-17 20:00:01 +0530

    complete project

--- mycart/settings.py
+++ mycart/settings.py
@@ -75,19 +75,22 @@ WSGI_APPLICATION = 'mycart.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-#
 # DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#             'NAME': 'ciba',
-#         }
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
+# }
+#s
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'postgres',
+            'USER': 'postgres',
+            'PASSWORD': 'anushruti@111',
+            'HOST': 'localhost'
+        }
+}
 
 
 # Password validation
